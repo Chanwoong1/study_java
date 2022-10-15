9ad403157b13643df12548ae95f518e37c51986a
Chanwoong1 <jcw7468@gmail.com>
2022-10-15 14:48:50 +0900

    10/15 java boj

---
+++ BOJ/gold/G5_1038/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Main {
+	static ArrayList<Long> list;
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		list = new ArrayList<>();
+		if (N <= 10) System.out.println(N);
+		else if (N > 1022) System.out.println("-1");
+		else {
+			for (int i = 0; i < 10; i++) {
+				bp(i, 1);
+			}
+			Collections.sort(list);
+			System.out.println(list.get(N));
+		}
+	}
+	
+	public static void bp(long num, int idx) {
+		if (idx > 10) return;
+		list.add(num);
+		for (int i = 0; i < num % 10; i++)
+			bp((num * 10) + i, idx + 1);
+	}
+}
